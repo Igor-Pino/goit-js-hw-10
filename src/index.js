63f7f4990a88c020187884da98f918e4e5f4416d
Igor-Pino <igorpinyo@gmail.com>
2021-08-27 12:24:57 +0300

    2.0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-import './partials/example'
+import './partials/templateMarkup'
+import './partials/theme'
--- src/partials/templateMarkup.js
+++ src/partials/templateMarkup.js
@@ -1,14 +1,9 @@
 import templetConstructor from './template.hbs';
 import menuItem from '../menu.json';
 
-console.log(menuItem)
-console.log(templetConstructor)
 
 const menuRef = document.querySelector('.js-menu')
-console.log(menuRef)
 
 const markup = templetConstructor(menuItem)
-console.log(markup)
-
 
 menuRef.insertAdjacentHTML('afterbegin', markup)
